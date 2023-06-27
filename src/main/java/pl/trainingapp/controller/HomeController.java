4dffc2ca3edd9ec9cddbcab57dc1f577dd239008
Mateusz <mateusz.ostro@o2.pl>
2023-06-27 22:25:02 +0200

    edited pom.xml and added controller

---
+++ src/main/java/pl/trainingapp/controller/HomeController.java
@@ -0,0 +1,15 @@
+package pl.trainingapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String getHome() {
+        return "home";
+    }
+}
