7255675a4db2288693bd9e86d521433b261a74dc
Mateusz <mateusz.ostro@o2.pl>
2023-08-16 22:36:24 +0200

    added new dtos

---
+++ src/main/java/pl/trainingapp/dtos/Exercise.java
@@ -0,0 +1,13 @@
+package pl.trainingapp.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Exercise {
+
+    int id;
+
+    String name;
+}
---
+++ src/main/java/pl/trainingapp/dtos/ExercisePlan.java
@@ -0,0 +1,11 @@
+package pl.trainingapp.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExercisePlan {
+
+    int id;
+}
